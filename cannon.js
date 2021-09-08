72091d807cf4099d2a8ae15f85c5f8ec33269b82
rishwanth-code <85011079+rishwanth-code@users.noreply.github.com>
2021-09-08 16:11:42 +0530

    Add files via upload

---
+++ cannon.js
@@ -0,0 +1,20 @@
+class Canon{
+    constructor(x,y, width, height, angle){
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.anglr = angle
+    
+    }
+
+    display(){
+        fill("grey")
+        push();
+        translate (this.x,this.y);
+        rotate (this.angle)
+        rect(-10,-20,this.width,this.height);
+        pop ()
+        arc(this.x-30,this.y+90,140,200,PI,TWO_PI)
+}
+}
